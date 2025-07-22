f34fe6940536e1ba761fa38bbb5301feeccd205d
laxman200456 <laxmancsuthar2004@gmail.com>
2025-07-22 14:30:01 +0530

    Project done

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss(),],
-  base: 'https://github.com/laxmancsuthar023/madam_ji_bday'
 })
